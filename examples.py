dbfe8c3e32c6c71c70c985b7f057234bfc78cadc
Murat Ardahan Aşık <muratardahanasik@gmail.com>
2021-08-16 23:37:17 +0300

    add DDPG example

--- examples.py
+++ examples.py
@@ -4,6 +4,7 @@ Contains example uses of provided RL agents.
 import gym
 import agents.dqn
 import agents.doubledqn
+import agents.ddpg
 
 def DQNexample():
     env = gym.make("LunarLander-v2")
@@ -58,7 +59,32 @@ def DoubleDQNexample():
         print(f"Episode {episode}, total reward {total_reward}, total steps {total_steps}")
 
 
+def DDPGexample():
+    env = gym.make("LunarLanderContinuous-v2")
+
+    agent = agents.ddpg.DDPGAgent(env=env, gamma=0.99, q_learning_rate=0.001, policy_learning_rate=0.0001,
+                                  batch_size=64, buffer_size=100000, noise=0.1, polyak=0.95)
+
+    episodes = 10000
+    for episode in range(episodes):
+        done = False
+        state = env.reset()
+        total_reward = 0
+        total_steps = 0
+        while not done:
+            action = agent.choose_action(state, training=True)
+            next_state, reward, done, info = env.step(action)
+            total_reward += reward
+            agent.store_transition(state, action, next_state, reward, done)
+            state = next_state
+
+            total_steps += 1
+            agent.train()
+
+        print(f"Episode {episode}, total reward {total_reward}, total steps {total_steps}")
+
+
 
 
 if __name__ == "__main__":
-    DoubleDQNexample()
+    DDPGexample()
